322710573435ed3c16296fdd4fab77f22d491e6d
Faqih Muntashir <itsfaqih@gmail.com>
2020-09-09 23:58:20 +0700

    Moved out image tag from HorizontalArticleItem to separate component

--- src/molecules/HorizontalArticleItem/index.jsx
+++ src/molecules/HorizontalArticleItem/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import HorizontalArticleBadge from '../../atoms/HorizontalArticleBadge';
 import HorizontalArticleTitle from '../../atoms/HorizontalArticleTitle';
-import './style.css';
+import HorizontalArticleThumbnail from '../../atoms/HorizontalArticleThumbnail';
 
 export default function HorizontalArticleItem({ title, url, badge, thumbnail }) {
   return (
     <div className="position-relative">
-      <img src={thumbnail} className="horizontal-article__thumbnail" alt={`Thumbnail ${title}`} />
+      <HorizontalArticleThumbnail src={thumbnail} />
       <HorizontalArticleBadge text={badge.text} type={badge.type} />
       <HorizontalArticleTitle text={title} href={url} />
     </div>
