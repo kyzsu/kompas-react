6dc59b54201dbb27566dbee4704c240022af5537
William Irsal <l0calh34rtz@gmail.com>
2020-09-10 12:21:02 +0700

    start again

--- src/atoms/headline-title/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./index.css";
-
-const HeadTitle = ({ text, type }) => {
-  return <h3 className={`text ${type}`}>{text}</h3>;
-};
-
-export default HeadTitle;
--- src/organism/headline/index.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import headImage from "../../molecules/headlineImage/index";
-import smallheadImage from "../../molecules/smallHeadlineImage/index";
-import "./index.css";
