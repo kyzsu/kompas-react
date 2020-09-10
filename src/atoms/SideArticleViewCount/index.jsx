f2ebc1cbe95acd343ab01f8ff0dca06996b0673a
Faqih Muntashir <itsfaqih@gmail.com>
2020-09-10 16:19:48 +0700

    Formatted view count and date time in article

--- src/atoms/SideArticleViewCount/index.jsx
+++ src/atoms/SideArticleViewCount/index.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import './style.css';
 
 export default function SideArticleViewCount({ count }) {
-  return <span className="side-article__view-count">Dibaca {count} kali</span>;
+  return <span className="side-article__view-count">Dibaca {Number(count).toLocaleString('id-ID')} kali</span>;
 }
--- src/atoms/VerticalArticleInfo/index.jsx
+++ src/atoms/VerticalArticleInfo/index.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import './style.css';
 
 export default function VerticalArticleInfo({ category, date }) {
+  const formattedDate = new Date(date).toLocaleString([], {
+    day: 'numeric',
+    month: 'numeric',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
+
   return (
     <div className="vertical-article__info">
       <span className="vertical-article__category">{category}</span>
-      <span className="vertical-article__date">{date}</span>
+      <span className="vertical-article__date">{formattedDate} WIB</span>
     </div>
   );
 }
