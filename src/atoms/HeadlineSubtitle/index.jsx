b2796e745238afbabe0909fd216a7fe1c454e0ec
William Irsal <l0calh34rtz@gmail.com>
2020-09-10 13:06:04 +0700

    Headline Finished

---
+++ src/atoms/HeadlineSubtitle/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+const HeadlineSubtitle = () => {
+  return <div className="Headline-Subtitle">"Headline"</div>;
+};
+
+export default HeadlineSubtitle;
--- src/atoms/text/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./index.css";
-
-const text = ({ text }) => {
-  return <p className={`text`}>{text}</p>;
-};
-
-export default text;
